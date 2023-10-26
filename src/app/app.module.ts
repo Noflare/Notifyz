e9df0f6e587c36de7ec09adfd15ce16d58c74e4c
Noflare <Noflare.dev@gmail.com>
2023-10-26 23:38:38 +0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import {NgOptimizedImage} from "@angular/common";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
+    imports: [
+        BrowserModule,
+        NgOptimizedImage,
+    ],
   declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
+    AppComponent,
+    NavComponent,
+    HomeComponent,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
+
 export class AppModule { }
