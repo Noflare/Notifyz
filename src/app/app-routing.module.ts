ebbaf4bc54c53d2027c6faf73ff912e03fbc09e3
Noflare <Noflare.dev@gmail.com>
2023-10-27 18:55:53 +0200

    Add phone and shop

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {ShopComponent} from "./shop/shop.component";
+import {HomeComponent} from "./home/home.component";
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'shop', component: ShopComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import {NgOptimizedImage} from "@angular/common";
 import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
     imports: [
         BrowserModule,
         NgOptimizedImage,
+        AppRoutingModule,
     ],
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
+    ShopComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
